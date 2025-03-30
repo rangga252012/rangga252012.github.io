116950dfe9d5b2f3d19124687831307b85782bf4
RNG25 <sofyantorangga025@gmail.com>
2025-03-30 13:48:25 +0700

    Delete Google directory

--- Google/script.js
+++
@@ -1,8 +0,0 @@
-function searchGoogle() {
-    var query = document.getElementById('searchInput').value;
-    window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(query);
-}
-
-function feelingLucky() {
-    window.location.href = 'https://www.google.com/doodles';
-}
