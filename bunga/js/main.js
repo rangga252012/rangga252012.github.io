25395e1b9ce47117279a8352983541a701e07ede
RNG25 <sofyantorangga025@gmail.com>
2025-03-30 23:13:56 +0700

    Update main.js

--- bunga/js/main.js
+++ bunga/js/main.js
@@ -1,17 +1,22 @@
-
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ['I LOVE U', 'YOU ARE AMAZING', 'NEVER GIVE UP', 'STAY POSITIVE', 'KEEP SMILING'];
     const titleElement = document.getElementById('title');
     let index = 0;
+    let titleIndex = 0;
 
     function appendTitle() {
-      if (index < titles.length) {
-        titleElement.innerHTML += titles[index];
+      if (index < titles[titleIndex].length) {
+        titleElement.innerHTML += titles[titleIndex][index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        setTimeout(appendTitle, 300); // 300ms delay
+      } else if (titleIndex < titles.length - 1) {
+        titleIndex++;
+        index = 0;
+        titleElement.innerHTML += ' '; // Add space between titles
+        setTimeout(appendTitle, 300); // 300ms delay
       }
     }
 
