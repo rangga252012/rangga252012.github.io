758eff1d907cc0a27d47cd089eec1f6a593f2497
RNG25 <sofyantorangga025@gmail.com>
2025-04-03 18:05:18 +0700

    Update index.js

--- Bocil/Selesai/js/index.js
+++ Bocil/Selesai/js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I happy birthday ita'.split('')
+const text = 'happy birthday ita'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
