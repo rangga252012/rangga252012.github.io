a7464bd60387eb15354c93f4395cfb013a38a8e1
RNG25 <sofyantorangga025@gmail.com>
2025-04-03 14:57:41 +0700

    Update main.js

--- Bocil/Selesai/js/main.js
+++ Bocil/Selesai/js/main.js
@@ -1,17 +1,22 @@
-
 onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('I LOVE U').split('');
+    const additionalTitles = ['A', 'B', 'C', 'D', 'E'];
     const titleElement = document.getElementById('title');
     let index = 0;
+    let additionalIndex = 0;
 
     function appendTitle() {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        setTimeout(appendTitle, 300); // 300ms delay
+      } else if (additionalIndex < additionalTitles.length) {
+        titleElement.innerHTML += additionalTitles[additionalIndex];
+        additionalIndex++;
+        setTimeout(appendTitle, 300); // 300ms delay
       }
     }
 
