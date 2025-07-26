39aa4c089dad0d7e7cefa1b080524e2d0ef31bc6
RNG25 <sofyantorangga025@gmail.com>
2025-07-26 18:12:57 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showModal(imgSrc, desc) {
-  document.getElementById('modalBg').style.display = 'flex';
-  document.getElementById('modalImg').src = imgSrc;
-  document.getElementById('modalDesc').innerText = desc;
-}
-function closeModal() {
-  document.getElementById('modalBg').style.display = 'none';
-}
