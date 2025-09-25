93941dbb6f9408016104244c6c7f5633443cf6e4
RNG25 <sofyantorangga025@gmail.com>
2025-09-25 17:08:21 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function showID(game, id) {
-  const display = document.getElementById('gameIdDisplay');
-  display.innerHTML = `<strong>${game} ID:</strong><br><span style="font-size:1.3em;">${id}</span>`;
-  display.classList.add('show');
-}
